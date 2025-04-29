fd6f03f5e43d3d94fdc5fb8d2ec529bd2cfbb106
dwiwahyufauzan <dwiwhy31@gmail.com>
2025-04-29 09:59:36 +0700

    setup deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
 const config = {
-  kit: {
-    adapter: adapter({
-      fallback: '404.html' // Supaya semua route dilayani
-    }),
-    paths: {
-      base: process.env.BASE_PATH || ''
-    }
-  }
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
+
 
 export default config;
